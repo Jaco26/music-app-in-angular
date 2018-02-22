30fc166c29cadc036c197b8e5d59a9c7be6abdc0
Jacob Albright <jacob.albright23@gmail.com>
2018-02-22 11:42:00 -0600

    controller is set up and angular is working

---
+++ server/public/scripts/angular-client.js
@@ -0,0 +1,11 @@
+const app = angular.module('musicApp', []);
+const musicController = app.controller('musicController', ['$http', musicCtl]);
+
+/////////////////// /////////////////// /////////////////// ////////
+// THIS IS THE musicController FUNCTION THAT DOES ALL THE STUFF ////
+/////////////////// /////////////////// /////////////////// ///////
+function musicCtl($http){
+    let self = this; // IMPORTANT...to preserve this meaning of "this" in the semantic variable, "self" 
+
+
+}; // END musicCtl()
